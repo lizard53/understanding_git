b52fc23d17ff68d382c1dfca07033477b8da4a98
pupun_windows <dharam.bhushan@outlook.com>
2017-03-27 22:01:21 -0700

    Revert "First change in feature branch"
    This reverts commit f2cc9cc92655f61cf7ccfe3756fefb799fa6ccd1.

--- test.py
+++ test.py
@@ -3,5 +3,4 @@ print "First_version_change in Mac"
 print "Second_version change in Mac"
 print "Change made from windows"
 print "One more Change made from mac"
-print "First change in feature branch"
 
